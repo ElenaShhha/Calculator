22a125dd937d8e997066c9a578faf88e4c292f0f
Elena Shabalina <elenapolkova@yandex.ru>
2025-03-04 14:46:01 +0500

    Update MainWindow.xaml.cs
    Исправила ошибку при делении на 0

--- Calculator/Calculator/MainWindow.xaml.cs
+++ Calculator/Calculator/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -36,11 +36,16 @@ namespace WpfApplication1
         private void СhangeOfSign_Click(object sender, RoutedEventArgs e)
         {
             string[] nums = disp.Split(mathSymbol);
-            string firstNum = nums[0].Replace(".", ","); // переменная храние значение - первое число на дисплее
+            string firstNum = nums[0].Replace(",", "."); // переменная храние значение - первое число на дисплее
             if (nums.Length == 2 && nums[1] != "") // если на дисплее 2 числа
             {
                 string secondNum = nums[1].Replace(".", ",");// переменная храние значение - второе число на дисплее
-                if (mathSymbol == '+') // если был знак плюс, то заменяем его на минус, так как + и - равно -
+         
+                if (secondNum.StartsWith("(-")) { // если надо убрать скоробочку, так как она уже есть
+                    secondNum = nums[nums.Length - 1].Replace("(", "").Replace(")", "").Replace("-", "");  
+                    disp = firstNum + mathSymbol + secondNum.ToString();
+                }
+                else if (mathSymbol == '+') // если был знак плюс, то заменяем его на минус, так как + и - равно -
                 {
                     mathSymbol = '–';
                     disp = firstNum + mathSymbol + secondNum.ToString();
@@ -50,30 +55,26 @@ namespace WpfApplication1
                     mathSymbol = '+';
                     disp = firstNum + mathSymbol + secondNum.ToString();
                 }
-                else disp = firstNum + mathSymbol + "(-" + secondNum.ToString() + ")";      // если был знак * или /, то знак не меняем, но добавляем во второму числу знак минуса и кнопки (- )                        
-            }
-            else if (nums.Length == 2 && nums[1] == "") // если на дисплее 1 число и заканчивается каким-то математическим знаком
-            {
-                disp = "(-" + firstNum.ToString() + ")" + mathSymbol;
-                display.Text = disp;
+                else { // если был знак * или /, то знак не меняем, но добавляем во второму числу знак минуса и кнопки (- )     
+                    disp = firstNum + mathSymbol + "(-" + secondNum.ToString() + ")"; 
+                }                        
             }
             else if (nums.Length == 1) // если на дисплее только одно число без знаков
             {
-                disp = "(-" + firstNum.ToString() + ")";
-                display.Text = disp;
+                if (nums[nums.Length - 1].StartsWith("(-")) disp = nums[nums.Length - 1].Replace("(", "").Replace(")", "").Replace("-", "");
+                else disp = "(-" + firstNum.ToString() + ")";
             }
             display.Text = disp;
+            Equality.Focus();
         }
 
-        private void Dot_Click(object sender, RoutedEventArgs e)
-        {
-            AddDot();
-        }
+        private void Dot_Click(object sender, RoutedEventArgs e) { AddDot(); }
 
         private void AddDot() // функция добавления точки 
         {
+            if (disp == "") { disp = "0";}
             if (disp[disp.Length - 1] == '.')
-            { // если уже есть 
+            { // если уже есть, то отмена точки
                 disp = disp.Remove(disp.Length - 1);
                 display.Text = disp;
                 return;
@@ -92,6 +93,7 @@ namespace WpfApplication1
             }
             else disp = disp + ".";
             display.Text = disp;
+            Equality.Focus();
         }
 
         private bool IsValidNumberLength(string lastNum) // функция проверки максимального кол-ва символов в числе
@@ -107,15 +109,24 @@ namespace WpfApplication1
             Button bt = (Button)sender;
             string num = bt.Content.ToString();
             AddNumber(num);
+            Equality.Focus();
         }
 
         private void AddNumber(string num) // функция добавления числа на дисплей
         {
+
             string[] nums = disp.Split(mathSymbol);
-            string[] Value = nums[nums.Length - 1].Split('.');
-            if (disp == "0") { disp = disp.Substring(0, disp.Length - 1); }
+            string[] Value = nums[nums.Length - 1].ToString().Replace(",", ".").Split('.');
+            if (disp == "0" ) { disp = disp.Substring(0, disp.Length - 1); }
             // по условию - на число не должно состоят более чем из 8 цифр до запятой и 3 после запятой
-            if (!string.IsNullOrEmpty(disp) && disp[disp.Length - 1] == ')' && Value.Length > 1 && (Value[1].Length < 4 || Value[0].Length < 10))
+            if (!string.IsNullOrEmpty(disp) && disp[disp.Length - 1] == ')' && Value.Length > 1 && Value[1].Length < 4)
+            {
+                disp = disp.Substring(0, disp.Length - 1); // удаляем последний символ
+                AddSymbolOnDisplay(num);
+                disp += ")";
+                display.Text = disp;
+            }
+            if (!string.IsNullOrEmpty(disp) && disp[disp.Length - 1] == ')' && Value.Length == 1 &&  Value[0].Length < 10)
             {
                 disp = disp.Substring(0, disp.Length - 1); // удаляем последний символ
                 AddSymbolOnDisplay(num);
@@ -123,7 +134,7 @@ namespace WpfApplication1
                 display.Text = disp;
             }
             else if (Value.Length > 1 && Value[1].Length < 3) AddSymbolOnDisplay(num);  // по условию - на число не должно состоят более чем из 3 цифр после запятой                  
-            else if (Value[0].Length < 8) AddSymbolOnDisplay(num); // по условию - на число не должно состоят более чем из 8 цифр до запятой                     
+            else if (Value.Length == 1 && Value[0].Length < 8) AddSymbolOnDisplay(num); // по условию - на число не должно состоят более чем из 8 цифр до запятой                     
         }
 
         private string MathOperation(double firstNum, double secondNum) // функция математических операций
@@ -158,6 +169,7 @@ namespace WpfApplication1
         {
             Button bt = (Button)sender;
             AddSymbol(bt.Content.ToString());
+            Equality.Focus();
         }
 
         private void AddSymbol(string bt)
@@ -186,10 +198,11 @@ namespace WpfApplication1
             AddSymbolOnDisplay(bt);
         }
 
-        private void Equality_Click(object sender, RoutedEventArgs e) { Calculation(); }
+        private void Equality_Click(object sender, RoutedEventArgs e) { Calculation();  }
 
         private void Calculation() // функция корректного отображения в textbox
         {
+            if (string.IsNullOrEmpty(disp)) return;
             string[] nums = disp.Split(mathSymbol);
             double firstNum = Convert.ToDouble(nums[0].Replace(".", ",").Replace("(", "").Replace(")", "")); // очищаем первое число от ( и ), меняет знак разделитель
 
@@ -208,11 +221,12 @@ namespace WpfApplication1
             {
                 disp = MathOperation(firstNum, lastNum);
             }
-
             nums = disp.Split(mathSymbol);
-            if (disp[0] == '-') disp = "(" + disp + ")"; // если нужно добавить скобочки
 
+            if (disp[0] == '-') disp = "(" + disp + ")"; // если нужно добавить скобочки
+            if (disp == "∞" || disp == "NaN" || disp == "не число") { disp = ""; }
             display.Text = disp;
+            Equality.Focus();
         }
 
         private void Window_KeyDown(object sender, KeyEventArgs e)
@@ -232,7 +246,7 @@ namespace WpfApplication1
             else if (e.Key == Key.Divide) AddSymbol("/");
             else if (e.Key == Key.Multiply) AddSymbol("*");
             else if (e.Key == Key.Decimal) { AddDot(); }
-            else if (e.Key == Key.Return) { Calculation(); }
+            else if (e.Key == Key.Enter ) { Calculation(); }
         }
     }
 }
